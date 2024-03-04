7cb75122e3f19ebd62b82354df56c7c0007d6c12
nacheung <nacheung@students.cs.ubc.ca>
2024-03-03 18:52:41 -0800

    Works with Sally's Baking and Love and Lemons

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let title, ingredients, instructions;
 function htmlDecodeWithLineBreaks($, html) {
     console.log(html);
     let myhtml = html
-    .replace(/<\/li>/gm, '\n</li>').replace(/<\/div>/gm, '</div>\n').replace(/<\/h1>/gm, '<\n').replace(/<script>(.|\n|\r)*?<\/script>/gm, '').replace(/<(?:.)*?>/gm, ''); // remove all html tags
+    .replace(/<\/li>/gm, '\n</li>').replace(/<\/div>/gm, '</div>\n').replace(/<\/h\d>/gm, '\n').replace(/<script>(.|\n|\r)*?<\/script>/gm, '').replace(/<(?:.)*?>/gm, ''); // remove all html tags
     return he.decode(myhtml);
   }
 
@@ -34,7 +34,6 @@ async function performScraping(url) {
     
     let title_element = $('.entry-title')[0];
     title = $(title_element).text();
-    console.log(title);
 
     let anchor = $('a').filter((index, element) => {
         return $(element).text().trim().toLowerCase() == "jump to recipe";
@@ -43,11 +42,13 @@ async function performScraping(url) {
 
     let text = htmlDecodeWithLineBreaks($, $(href).html());
 
-    let starting_at_ingredients = text.replace(new RegExp('(.|\n)*(\t|\n|\s|\r)+Ingredients(\t|\n|\s|\r)+'), '');
+    let textWithNoExtraLineBreaks = text.replace(/(\r?\n\t?\s?){3,}/g, '\n');
+
+    let starting_at_ingredients = textWithNoExtraLineBreaks.replace(new RegExp('(.|\n)*(\t|\n|\s|\r)+Ingredients(\t|\n|\s|\r)*'), '');
     starting_at_ingredients.trimStart();
-    ingredients = starting_at_ingredients.replace(new RegExp('(\t|\n|\s|\r)+Instructions(.|\n|\r)*'), '');
-    let starting_at_instructions = text.replace(new RegExp('(.|\n)*(\t|\n|\s|\r)+Instructions(\t|\n|\s|\r)+'), '');
-    instructions = "-" + starting_at_instructions.replace(/\n(?!(\n|\r|\t|\s)+)/g, '\n -').replace(/(\r?\n\t?\s?){3,}/g, ''); // add dashes and remove extra line breaks
+    ingredients = starting_at_ingredients.replace(new RegExp('(\t|\n|\s|\r)*Instructions(.|\n|\r)*'), '').replace(/Cook Mode(.|\n|\r)*Prevent your screen from going dark/g, '');
+    let starting_at_instructions = text.replace(new RegExp('(.|\n)*(\t|\n|\s|\r)*Instructions(\t|\n|\s|\r)*'), '');
+    instructions = "-" + starting_at_instructions.replace(/\n(?!(\n|\r|\t|\s)+)/g, '\n -'); 
 
 }
 
